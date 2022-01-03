229283d4759c43189c3b348a3d0751bca1ccbc08
MilanMarakana <milan.marakana7@gmail.com>
2022-01-04 00:17:45 +0100

    add apollo container for collections.component

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,15 +1,14 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
 
 const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
+  apiKey: 'AIzaSyCcGt1B7Pwy7TItcOpQ24yUnv90JV_MA-4',
+  authDomain: 'crown-db-c2f7a.firebaseapp.com',
+  projectId: 'crown-db-c2f7a',
+  storageBucket: 'crown-db-c2f7a.appspot.com',
+  messagingSenderId: '255227614361',
+  appId: '1:255227614361:web:f52aae3a4bed3543981408',
 };
 
 firebase.initializeApp(config);
